2130173a27a45ae9bed52e17400f1f088378ad17
Csaki Boldizsar <csakiboldizsar@Csakis-iMac.local>
2018-12-14 11:54:27 +0100

    subject service

---
+++ src/app/services/subject.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { httpOptions } from './auth.service'
+import { Subject } from '../classes/subject';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SubjectService {
+
+  private subjectUrl = 'http://localhost:8080/subjects';
+
+  constructor(private http: HttpClient) { }
+
+  getSubjects(): Promise<Subject[]>{
+    return this.http.get<Subject[]>(`${this.subjectUrl}`,httpOptions).toPromise();
+  }
+  getSubject(id: number): Promise<Subject>{
+    return this.http.get<Subject>(`${this.subjectUrl}/${id}`,httpOptions).toPromise();
+  }
+  createSubject(subject: Subject): Promise<Subject>{
+    return this.http.post<Subject>(`${this.subjectUrl}`,{
+
+    },httpOptions).toPromise();
+  }
+  addLecturer(id: number): Promise<Subject>{
+    return this.http.put<Subject>(`${this.subjectUrl}/${id}/add-lecturer`,{
+      //
+    },httpOptions).toPromise();
+  }
+  removeLecturer(id:number): Promise<Subject>{
+    return this.http.put<Subject>(`${this.subjectUrl}/${id}/remove-lecturer`,{
+
+    },httpOptions).toPromise();
+  }
+  addCourse(id: number): Promise<Subject>{
+    return this.http.put<Subject>(`${this.subjectUrl}/${id}/add-course`,{
+      //
+    },httpOptions).toPromise();
+  }
+  removeCourse(id: number): Promise<Subject>{
+    return this.http.put<Subject>(`${this.subjectUrl}/${id}/remove-course`,{
+      //
+    },httpOptions).toPromise();
+  }
+  deleteSubject(id: number): Promise<Subject>{
+    return this.http.delete<Subject>(`${this.subjectUrl}/${id}`,httpOptions).toPromise();
+  }
+}
