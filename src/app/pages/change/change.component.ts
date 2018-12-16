2be193de2c5694047168897171d4f7cf02460adc
Hamumus <cs.boldi@gmail.com>
2018-12-16 15:58:00 +0100

    modositas aloldal

--- src/app/pages/change/change.component.ts
+++ src/app/pages/change/change.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CourseService } from 'src/app/services/course.service';
+import { Course } from 'src/app/classes/course';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-change',
@@ -7,9 +11,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChangeComponent implements OnInit {
 
-  constructor() { }
+  course: Course;
+  registerForm: FormGroup;
+  submitted = false;
 
-  ngOnInit() {
+  constructor(private route: ActivatedRoute,private courseService: CourseService,private formBuilder: FormBuilder,private router: Router,) { }
+
+  async ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      location: [],
+      time: [],
+      type: [],
+    });
+    const id = parseInt(this.route.snapshot.paramMap.get('id'));
+    console.log(id);
+    this.course = await this.courseService.getCourse(id);
+    console.log(this.course);
+
+    
   }
+  get f() { return this.registerForm.controls; }
+
+  onSubmit() {
+    this.submitted = true;
+
+
+    if (this.registerForm.invalid) {
+      //console.log("invalid");
+      return;
+    }
+
+    //console.log(this.registerForm.value);
+    //this.courseService.createCourse(this.registerForm.value);
+    // elkuldeni a cuccokat
+    //this.router.navigate(['/courselist']);
+
+
+  }
+
 
 }
